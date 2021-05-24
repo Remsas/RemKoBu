d3cb07e489dab0349c12b7ee6e9d7f0d48e66c39
Remo <r.basteck@gmail.com>
2021-05-24 17:18:43 +0200

    create console project to try out

---
+++ try-out/RemKoBu.TryOutCon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemKoBu.TryOutCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
